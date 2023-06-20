0777956b670fd1286b2931f5cee69a1428672aee
John_Lamy <johnclamy@gmail.com>
2023-06-20 17:23:17 +0100

    init commit

---
+++ pages/contact-us.jsx
@@ -0,0 +1,11 @@
+export default function ContactUS() {
+  return (
+    <div>
+      <ul>
+        <li>Email: jon-doe445@homeworkers-inc.com</li>
+        <li>Twitter: @jon-doe445</li>
+        <li>Instagram: jon-doe445</li>
+      </ul>
+    </div>
+  );
+}
---
+++ pages/index.jsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <div>Welcome to the blog</div>
+  )
+}
