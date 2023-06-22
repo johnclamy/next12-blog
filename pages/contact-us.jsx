0e319cd6d121d9dd29204a548328c797bda97b5a
John_Lamy <johnclamy@gmail.com>
2023-06-22 10:23:10 +0100

    add Head component to contacts page

--- pages/contact-us.jsx
+++ pages/contact-us.jsx
@@ -1,11 +1,29 @@
+import Head from "next/head";
+import Link from "next/link";
+import Widget from "../components/Widget";
+
 export default function ContactUS() {
   return (
     <div>
+      <Head>
+        <title>You can contact us</title>
+      </Head>
+      <div>
+        <Link href="/about-us" passHref>
+          <a>About Us</a>
+        </Link>
+        <Link href="/" passHref>
+          <a>Back to home</a>
+        </Link>
+      </div>
       <ul>
         <li>Email: jon-doe445@homeworkers-inc.com</li>
         <li>Twitter: @jon-doe445</li>
         <li>Instagram: jon-doe445</li>
       </ul>
+      <div>
+        <Widget pageName="contact" />
+      </div>
     </div>
   );
 }
