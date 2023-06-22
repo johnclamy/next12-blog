e413c4c3c9e8f29cfbfb55574f9a2b8dd5d18378
John_Lamy <johnclamy@gmail.com>
2023-06-22 23:57:39 +0100

    add the theme context provider to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,25 @@
+import { useState } from 'react';
+import ThemeContext from '../contexts/themeContext';
 import Navbar from '../components/Navbar.'
+import themes from '../styles/themes'
 import '../styles/global.css'
 
 export default function MyApp({ Component, pageProps }) {
+  const [theme, setTheme] = useState(themes.light)
+  const toggleTheme = () => setTheme(
+    theme === 'dark' ? 'light' : 'dark'
+  )
+
   return (
-    <>
-      <Navbar />
-      <Component {...pageProps} />
-    </>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div style={{
+        width: '100%',
+        minHeight: '100vh',
+        ...themes[theme]
+      }}>
+        <Navbar />
+        <Component {...pageProps} />
+      </div>
+    </ThemeContext.Provider>
   );
 }
