774338be9025c1f1bcaaf07cccdd5b40fae12110
John_Lamy <johnclamy@gmail.com>
2023-06-23 13:47:53 +0100

    setup the _document.js file

---
+++ pages/_document.js
@@ -0,0 +1,25 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript
+} from 'next/document'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initalProps = await Document.getInitialProps(ctx)
+    return {
+      ...initalProps
+    }
+  }
+
+  render() {
+    return <Html>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  }
+}
