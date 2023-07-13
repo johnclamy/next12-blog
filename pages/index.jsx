5668422f69934b9f0475ae5181aa90a3c6173ea1
John_Lamy <johnclamy@gmail.com>
2023-07-13 15:26:48 +0100

    refactor Home component to HomePage

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,4 @@
-import Head from 'next/head'
-import Link from 'next/link';
+import HomePage from "../components/templates/HomePage";
 
 const posts = [
   { title: 'The state of Crypto 2023', excerpt: 'It\'s been a tough year for crypto investors' },
@@ -7,20 +6,5 @@ const posts = [
 ]
 
 export default function Home() {
-  return (
-    <div className="container mx-auto px-2 sm:px-10 mb-8">
-      <Head>
-        <title>Bloggerz Home page | Welcome to Bloggerz</title>
-        {/* <link rel='icon' href='/favicon.ico' /> */}
-      </Head>
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        {posts.map(post =>
-          <div key={post.title}>
-            {post.title}
-            {post.excerpt}
-          </div>
-        )}
-      </div>
-    </div>
-  );
+  return <HomePage posts={posts} />
 }
