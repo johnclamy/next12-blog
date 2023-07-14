bd0f898d1fc3320d449888597acb84ef5f0c5b19
John_Lamy <johnclamy@gmail.com>
2023-07-14 22:00:45 +0100

    wrap the app component with the custom layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
+import Layout from '../components/organisms/Layout';
 import '../styles/global.scss'
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <Component {...pageProps} />
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
