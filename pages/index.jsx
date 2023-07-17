f3d930f8a67335c25d043f855d5af50ef24849ff
John_Lamy <johnclamy@gmail.com>
2023-07-17 14:16:05 +0100

    pass fetched data from the api as props in Home

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,20 +1,14 @@
 import { getPosts } from "../lib/graphql";
 import HomePage from "../components/templates/HomePage";
 
-const posts = [
-  { title: 'The state of Crypto 2023', excerpt: 'It\'s been a tough year for crypto investors' },
-  { title: 'Happy consensus', excerpt: 'Good things come to those who wait. But it\'s been a long long wait!' }
-]
-
-export default function Home({ _posts }) {
-  console.log('On the home page:', _posts)
+export default function Home({ posts }) {
   return <HomePage posts={posts} />
 }
 
 // Fetch data at build time
 export async function getStaticProps() {
-  const _posts = (await getPosts()) || []
+  const posts = (await getPosts()) || []
   return {
-    props: { _posts }
+    props: { posts }
   }
 }
