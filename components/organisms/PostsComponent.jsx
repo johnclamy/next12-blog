935192913811f84c4db01d7bbe8214bfdb6cbf2e
John_Lamy <johnclamy@gmail.com>
2023-07-17 14:09:21 +0100

    set the post card component to accept post data

--- components/organisms/PostsComponent.jsx
+++ components/organisms/PostsComponent.jsx
@@ -3,7 +3,7 @@ import PostCard from "../molecules/PostCard";
 export default function PostsComponent({ posts }) {
   return (
     <article className="lg:col-span-8 col-span-1">
-      {posts.map((post) => <PostCard post={post} />)}
+      {posts.map((post) => <PostCard key={post.node.slug} post={post.node} />)}
     </article>
   );
 }
