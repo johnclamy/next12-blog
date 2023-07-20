90ecacc01250b344810e9b193601a7deef5e9dd0
John_Lamy <johnclamy@gmail.com>
2023-07-20 16:20:50 +0100

    reduce the size of the logo on sm screens

--- components/molecules/Logo.jsx
+++ components/molecules/Logo.jsx
@@ -4,7 +4,7 @@ export default function Logo() {
   return (
     <section className='md:float-left block'>
       <Link href='/'>
-        <b className='cursor-pointer font-bold text-4xl text-yellow-900 capitalize'>
+        <b className='cursor-pointer font-bold text-2xl text-yellow-900 capitalize'>
           bloggerz
         </b>
       </Link>  
